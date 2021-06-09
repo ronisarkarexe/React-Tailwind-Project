0448c747fa3dbcc52a530fd407010d12267ee714
Aomi Sarkar <ronisarkar2244@gmail.com>
2021-06-09 20:28:38 +0600

    React Tailwind

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FreeGet from './component/FreeGet';
+import HomeCart from './component/HomeCart';
+import Services from './component/Services';
+
+function App() {
+
+
+  return (
+    <div>
+        <HomeCart></HomeCart>
+        <Services></Services>
+        <FreeGet></FreeGet>
+    </div>
+  );
+}
+
+export default App;
